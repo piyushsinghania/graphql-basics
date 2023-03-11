1860cad95bd12f612eeac3e9c1ddaeabe5e17f38
piyushsinghania <piyushsinghania28@gmail.com>
2023-03-11 21:28:51 +0530

    Added comment create mutation

--- src/index.js
+++ src/index.js
@@ -89,6 +89,7 @@ const typeDefs = `
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
     createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
 
   type User {
@@ -179,7 +180,7 @@ const resolvers = {
     createPost(parent, args, ctx, info) {
       const userExists = users.some(user => user.id === args.author)
       if(!userExists) {
-        throw new Error("User doesn't exists")
+        throw new Error("User not found")
       }
 
       const post = {
@@ -192,6 +193,28 @@ const resolvers = {
 
       posts.push(post)
       return post
+    },
+    createComment(parent, args, ctx, info) {
+      // check for user
+      const userExists = users.some(user => user.id === args.author)
+      if(!userExists) {
+        throw new Error("User not found")
+      }
+
+      // check for post
+      const postExists = posts.some(post => post.id === args.post && post.published)
+      if(!postExists) {
+        throw new Error("Post not found")
+      }
+
+      const comment = {
+        id: v4(),
+        text: args.text,
+        author: args.author,
+        post: args.post
+      }
+      comments.push(comment);
+      return comment;
     }
   },
   Post: {
