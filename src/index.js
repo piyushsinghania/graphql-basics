1635c6d4512c327fa1da9b7f9b2122fa84ec270c
piyushsinghania <piyushsinghania28@gmail.com>
2023-03-11 16:39:42 +0530

    Added mutation for createUser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from 'graphql-yoga';
+import { v4 } from 'uuid';
 
 // Dummy data
 const users = [
@@ -85,6 +86,10 @@ const typeDefs = `
     comments: [Comment!]!
   }
 
+  type Mutation {
+    createUser(name: String!, email: String!, age: Int): User!
+  }
+
   type User {
     id: ID!
     name: String!
@@ -151,6 +156,24 @@ const resolvers = {
     },
     comments(parent, args, ctx, info) {
       return comments
+    }
+  },
+  Mutation: {
+    createUser(parent, args, ctx, info) {
+      const emailTaken = users.some(user => user.email === args.email)
+      if (emailTaken) {
+        throw new Error('Email already registered.')
+      }
+
+      const user = {
+        id: v4(),
+        name: args.name,
+        email: args.email,
+        age: args.age
+      }
+
+      users.push(user)
+      return user
     }
   },
   Post: {
