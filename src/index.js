9c794d76367ddb05bcc1f799b512b40ff693ca29
piyushsinghania <piyushsinghania28@gmail.com>
2023-03-11 13:43:35 +0530

    Added author to comment

--- src/index.js
+++ src/index.js
@@ -100,6 +100,7 @@ const typeDefs = `
     body: String!
     published: Boolean!
     author: User!
+    comments: [Comment!]!
   }
 
   type Comment {
@@ -155,6 +156,9 @@ const resolvers = {
   Post: {
     author(parent, args, ctx, info) {
       return users.find((user) => user.id === parent.author)
+    },
+    comments(parent, args, ctx, info) {
+      return comments.filter((comment) => comment.post === parent.id)
     }
   },
   User: {
